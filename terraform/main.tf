4f3e131c2e14396a49e912c155e8e17f466b3701
Sudheer-Ambati95 <164061222+Sudheer-Ambati95@users.noreply.github.com>
2025-03-02 08:24:45 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "devops_bucket" {
+  bucket = "your-devops-bucket"
+}
+
+resource "aws_iam_role" "jenkins_role" {
+  name = "jenkins-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
